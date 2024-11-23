1b868f2615d9ddc5f70f33b3d75e74562a44b5f1
AlejoRam05 <emmanuel.alej15@gmail.com>
2024-11-23 15:59:09 -0300

    Modelo de base de datos y respuesta de la API, siguendo el protocolo APIRest

--- models/user_model.py
+++ models/user_model.py
@@ -8,11 +8,41 @@ class Cliente(BaseModel):
     username: str
     email: EmailStr
     password: str
+    roles: bool = Field(default=False)
+
+class Admin(BaseModel):
+
+    username: str
+    email: EmailStr
+    password: str
+
+
+class ClienteResponse(BaseModel):
+
+    username: str
+    email: EmailStr
+    
+
+
+class AdminResponse(BaseModel):
+
+    username: str
+    email: EmailStr
+    roles: bool
+
 
 class ClienteDB(SQLModel, table = True):
 
     id: Optional[int] = Field(primary_key=True, default=None)
-    username: str
+    username: str = Field(unique=True)
     email: str = Field(index=True, unique=True)
     password: str  
     roles: bool = Field(default= False)
+
+class AdminDB(SQLModel, table = True):
+
+    id: Optional[int] = Field(primary_key=True, default=None)
+    username: str = Field(unique=True)
+    email: str = Field(index=True, unique=True)
+    password: str  
+    roles: bool = Field(default= True)
