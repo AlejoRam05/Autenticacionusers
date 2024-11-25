666d222901af73c1ac4d37a0d51f4174df68c6d0
AlejoRam05 <emmanuel.alej15@gmail.com>
2024-11-25 20:52:22 -0300

    Creacion de rutas de bienvenidas por roles

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -1,22 +1,38 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError
+from typing import Annotated
+from sqlmodel import Session
+from db import get_session
+from models.user_model import AdminDB,ClienteDB
+from utils.jwt_utils import verify_jwt
 
 router = APIRouter(prefix="/dashboard")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+@router.get("/admin", status_code=200)
+async def dashboard_admin(token: Annotated[str, Depends(oauth2_scheme)], db_admin: Session = (Depends(get_session))):
+    try:
+        data_verify = verify_jwt(token)
+        if data_verify:
+            data_admin = db_admin.query(AdminDB).all()
+            data_clientes = db_admin.query(ClienteDB).all()
+            return {"admin": data_admin, "clientes": data_clientes}
+    except HTTPException as e:
+        raise e
 
-@router.get("/admin")
-async def dashboard_admin():
-    pass
+@router.get("/user/{username}", status_code=200)
+async def dashboard_user(username: str, token: Annotated[str, Depends(oauth2_scheme)], db_cliente: Session = Depends(get_session)):
+    try:
+        data_verify = verify_jwt(token)
+        if data_verify:
+            data_user = db_cliente.query(ClienteDB).filter(ClienteDB.username == username).first()
+            if not data_user:
+                raise HTTPException(status_code=404, detail="User not found")
+            return {"username": data_user.username}
 
-@router.put("/admin")
-async def logout_admin():
-    pass
+    except HTTPException as e:
+        raise e
 
-@router.get("/user")
-async def dashboard_user():
-    pass
-
-@router.put("/user")
-async def logout_user():
-    pass
 
 
